e0b74d50cb12ee06c30685f1224f2593df19c57d
nusratullo_a <axunovnusrat@gmail.com>
2024-07-02 23:56:46 +0500

    logut

--- src/layouts/components/Sidebar.vue
+++ src/layouts/components/Sidebar.vue
@@ -3,118 +3,7 @@
         <header
             class="flex items-center relative h-5 gap-5 justify-between w-full text-base font-medium tracking-tighter text-black whitespace-nowrap"
         >
-            <el-popover placement="bottom-start" popper-class="!top-[60px]"  :width="181" trigger="click">
-                <template #reference>
-                    <div class="flex items-center gap-1 cursor-pointer">
-                        <LogoIcon class="w-[18px]" />
-                        <span class="text-base text-black font-medium">Opus</span>
-                        <Icon size="8" color="#939496" name="el-icon-ArrowDown" />
-                    </div>
-                </template>
-                <div>
-                    <el-popover placement="right-start" :width="240" trigger="click">
-                        <template #reference>
-                            <div class="flex items-center justify-between route">
-                                <div class="flex items-center gap-1.5">
-                                    <div
-                                        class="w-[14px] h-[14px] bg-black rounded-full flex items-center justify-center text-[6px] font-inter pt-0.5 text-white"
-                                    >
-                                        IA
-                                    </div>
-                                    {{ user.name }}
-                                </div>
-                                <Icon size="8" color="#343330" name="el-icon-ArrowRightBold" />
-                            </div>
-                        </template>
-                        <div class="flex items-center gap-1.5 route">
-                            <div
-                                class="w-[14px] h-[14px] bg-black rounded-full flex items-center justify-center text-[6px] leading-[0px] font-inter text-white pt-0.5"
-                            >
-                                IA
-                            </div>
-                            {{ user.email }}
-                        </div>
-                        <hr />
-                        <div class="flex items-center gap-1.5 route">
-                            <div
-                                class="w-[14px] h-[14px] bg-black rounded-full flex items-center justify-center text-[6px] leading-[0px] font-inter text-white pt-0.5"
-                            >
-                                IA
-                            </div>
-                            i.akramov@ai-lab.uz
-                        </div>
-                        <hr />
-                        <div class="flex items-center gap-1.5 route">
-                            <div
-                                class="w-[14px] h-[14px] bg-black rounded-full flex items-center justify-center text-[6px] leading-[0px] font-inter text-white pt-0.5"
-                            >
-                                IA
-                            </div>
-                            i.akramov@centralasian.uz
-                        </div>
-                        <div class="flex items-center gap-1.5 route !text-[#9D9D9F]">
-                            <Icon size="12" class="mb-0.5" color="#343330" name="el-icon-Plus" />
-                            Add new account
-                        </div>
-                        <hr />
-                        <div class="flex items-center gap-1.5 route !text-[#FF0000] !bg-[#FFE8E8]">
-                            <Icon size="14" class="mb-0.5" color="#FF0000" name="el-icon-Plus" />
-                            Log out
-                        </div>
-                    </el-popover>
-                    <el-popover placement="right-start" :width="240" trigger="click">
-                        <template #reference>
-                            <div class="flex items-center justify-between route">
-                                <div class="flex items-center gap-1.5">
-                                    <Icon size="14" color="#5C5C5E" name="local-switch" />
-                                    Switch workspace
-                                </div>
-                                <Icon size="8" color="#343330" name="el-icon-ArrowRightBold" />
-                            </div>
-                        </template>
-                        <div class="flex items-center gap-1.5 route !py-2.5 !text-[#444444]">
-                            <div
-                                class="w-[24px] h-[24px] border bg-[#2F3640] rounded-full flex items-center justify-center text-[9px] leading-[0px] font-inter text-white"
-                            >
-                                AI
-                            </div>
-                            AI Lab
-                        </div>
-                        <hr />
-                        <div class="flex items-center gap-1.5 !py-2.5 route !text-[#444444]">
-                            <div
-                                class="w-[24px] h-[24px] border bg-[#2F3640] rounded-full flex items-center justify-center text-[9px] leading-[0px] font-inter text-white"
-                            >
-                                P
-                            </div>
-                            Private projects
-                        </div>
-                        <hr />
-                        <div class="flex items-center gap-1.5 route !text-[#9D9D9F]">
-                            <Icon size="12" class="mb-0.5" color="#343330" name="el-icon-Plus" />
-                            Add new workspace
-                        </div>
-                    </el-popover>
-                    <hr />
-                    <div class="flex items-center gap-1.5 route">
-                        <Icon size="14" color="#343330" name="local-setting" />
-                        Workspace settings
-                    </div>
-                    <div class="flex items-center gap-1.5 route">
-                        <Icon size="14" color="#343330" name="local-users" />
-                        Manage members
-                    </div>
-                    <div class="flex items-center gap-1.5 route">
-                        <Icon size="14" color="#343330" name="local-credit" />
-                        Billing
-                    </div>
-                    <hr />
-                    <div class="flex items-center gap-1.5 route">
-                        <Icon size="14" color="#343330" name="local-help" />
-                        Help
-                    </div>
-                </div>
-            </el-popover>
+            <userSettings :user="user" @logut="logOut" />
             <button>
                 <Icon size="21" color="#343330" name="local-sidebar-simple" />
             </button>
@@ -198,11 +87,13 @@
   <script lang="ts" setup>
 import { computed, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import LogoIcon from '/@/components/icons/LogoIcon.vue'
+import userSettings from './userSettings.vue'
+import type { Action } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus'
 import { useUsersStore } from '/@/stores'
 const router = useRouter()
 const usersStore = useUsersStore()
-const user = computed(() => JSON.parse(usersStore.getUser))
+const user = computed(() => (typeof usersStore.getUser == 'string' ? JSON.parse(usersStore.getUser) : usersStore.getUser))
 const currentRoute = computed(() => router.currentRoute.value.fullPath || 'default')
 const mainMenus = ref([
     {
@@ -320,6 +211,21 @@ const subMenus: any = ref([
         ],
     },
 ])
+const logOut = async () => {
+    ElMessageBox.confirm('Are you sure logOut. Continue?', 'Warning', {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
+        type: 'warning',
+        draggable: true,
+    })
+        .then(() => {
+            usersStore.resetToken()
+            router.push('/signin')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
 </script>
 <style scoped>
 .nested-dropdown-menu {
--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -3,43 +3,47 @@
         <h4 class="text-[#0A1217] font-medium text-[28px] leading-7">Hello, {{ user.name }}</h4>
         <p class="text-[#0A121740] font-medium text-[25px]">How can I help you today?</p>
         <!-- <el-scrollbar height="100%" class="w-[80vw]"> -->
-            <div class="grid grid-cols-3 xl:grid-cols-4 gap-6 mt-9">
-                <IssueCreationCard v-for="(data, i) in issues" :key="i" :data="data" />
-            </div>
+        <div class="grid grid-cols-3 xl:grid-cols-4 gap-6 mt-9">
+            <IssueCreationCard v-for="(data, i) in issues" :key="i" :data="data" />
+        </div>
         <!-- </el-scrollbar> -->
     </div>
 </template>
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import IssueCreationCard from './components/IssueCreationCard.vue'
 import { useUsersStore } from '/@/stores'
 
 const usersStore = useUsersStore()
-const user = computed(() => JSON.parse(usersStore.getUser))
+const user = computed(() => (typeof usersStore.getUser == 'string' ? JSON.parse(usersStore.getUser) : usersStore.getUser))
+
 const issues = [
     {
         command: 'Create issue',
         title: 'Create a new issue for any team and project',
         co: 'i',
-        icon: 'scan'
+        icon: 'scan',
     },
     {
         command: 'Create team',
         title: 'Create new team for collaboration work',
         co: 't',
-        icon: 'cross'
+        icon: 'cross',
     },
     {
         command: 'Create project',
         title: 'Create new projects for teams',
         co: 'p',
-        icon: 'teams'
+        icon: 'teams',
     },
     {
         command: 'Create milestone',
         title: 'Create new milestone for better productivity of team',
         co: 'c',
-        icon: 'scan'
+        icon: 'scan',
     },
 ]
+onMounted(() => {
+    console.log(typeof usersStore.getUser)
+})
 </script>
